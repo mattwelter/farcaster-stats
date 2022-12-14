8549c3788922592f832f355cecc7d26b97113f26
mattwelter <welter.m@icloud.com>
2022-12-14 14:34:43 -0700

    Real v1

--- index.js
+++ index.js
@@ -9,31 +9,12 @@ const clusterUrl = process.env.CLUSTERURL
 
 const uri = `mongodb+srv://${username}:${password}@${clusterUrl}?w=majority&authMechanism=DEFAULT`;
 const client = new MongoClient(uri);
-
 console.log(uri)
 
-async function run() {
-  try {
-    await client.connect();
-    const database = client.db("Farcaster");
-    const profiles = database.collection("profiles");
-    const cursor = profiles.find();
-    await cursor.forEach(doc => console.dir(doc));
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
+client.connect(err => {
+  const collection = client.db("Farcaster").collection("profiles");
 
+  // perform actions on the collection object
 
-
-
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://kay:myRealPassword@cluster0.mongodb.net/test?w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//  // perform actions on the collection object
-//   client.close();
-// });
+  client.close();
+});
